67151260dc9240b9d8e29b2b6f6b9b6dfe06cd9b
sean-rw <seanrwhitley@gmail.com>
2022-03-20 13:46:11 -0700

    Styling, refactoring, documentation efforts

--- src/modules/my/listContainer/listContainer.js
+++ src/modules/my/listContainer/listContainer.js
@@ -2,31 +2,13 @@ import { LightningElement, api, track } from 'lwc';
 // import { refreshApex } from "@salesforce/apex";
 // import { getListOf } from "@salesforce/apex/ProfileHelper.getListOf";
 
-
-function getMockData(amount) {
-    let data = new Array();
-    for (let i = 0; i < amount; i++) {
-        data.push({
-            Id: randomId(),
-            Name: 'Test',
-            Date: '12/12/1212',
-            Detail: 'TEstT Est aTEs'
-        })
-    }
-
-    return data;
-}
-
-function randomId() {
-    return String(Math.floor(Math.random() * 10000000));
-}
-
-
 export default class ListContainer extends LightningElement {
 
     @track list;
 
     get type() {
+        // Implemented types:
+        //  Education/Experience
         return this._type;
     }                    
     
@@ -43,10 +25,10 @@ export default class ListContainer extends LightningElement {
                 console.log('Unsupported list type: ' + input)
                 break;
         }
-    }                      // Education/Experience
-    isEditing = false;              // 
-    isCreating = false;             // 
-
+    }                      
+    isEditing = false;          // 
+    isCreating = false;         // 
+    editingId;                  // RecordId selected for editing
 
     connectedCallback() {
         this.list = getMockData(5);
@@ -64,14 +46,48 @@ export default class ListContainer extends LightningElement {
         this.list = data;
     }
 
+    // Listens for onclick event on record-edit-form submit button
+    handleSubmitFormButton() {
+        // When record created/edited, refresh list to show changes
+        // refreshApex(this.list);
+
+        // Reset state
+        this.isEditing = false;
+        this.isCreating = false;
+        this.editingId = '';
+    }
+
     // Handle displaying the create modal form
-    onAddNewRowHandler() {
+    handleCreateRecordButton() {
         this.isCreating = true;
     }
 
-    // When a record is edited/added, refresh list
-    // Listens for onclick event on record-edit-form submit button
-    onSubmitHandler() {
-        // refreshApex(this.list);
+    // Capture event from child row component
+    handleEditRecordEvent(event) {
+        this.editingId = event.detail.Id;
+        this.isEditing = true;
     }
+}
+
+
+/**
+ *  Mock Data for Prototyping.
+ *  TODO: delete me!
+ */
+function getMockData(amount) {
+    let data = new Array();
+    for (let i = 0; i < amount; i++) {
+        data.push({
+            Id: randomId(),
+            Name: 'Test',
+            Date: '12/12/1212',
+            Detail: 'TEstT Est aTEs'
+        })
+    }
+
+    return data;
+}
+
+function randomId() {
+    return String(Math.floor(Math.random() * 10000000));
 }
