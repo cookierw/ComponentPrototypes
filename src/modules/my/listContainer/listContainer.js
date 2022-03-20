b6b45a647183c69a0b17e326510df0e8c9af90b9
sean-rw <seanrwhitley@gmail.com>
2022-03-20 12:46:36 -0700

    Properties and get/setter for type public property with validation

--- src/modules/my/listContainer/listContainer.js
+++ src/modules/my/listContainer/listContainer.js
@@ -1,18 +1,61 @@
-import { LightningElement, api, track, wire } from 'lwc';
-import { refreshApex } from "@salesforce/apex";
-import { getListOf } from "@salesforce/apex/ProfileHelper.getListOf";
+import { LightningElement, api, track } from 'lwc';
+// import { refreshApex } from "@salesforce/apex";
+// import { getListOf } from "@salesforce/apex/ProfileHelper.getListOf";
+
+
+function getMockData(amount) {
+    let data = new Array();
+    for (let i = 0; i < amount; i++) {
+        data.push({
+            Id: randomId(),
+            Name: 'Test',
+            Date: '12/12/1212',
+            Detail: 'TEstT Est aTEs'
+        })
+    }
+
+    return data;
+}
+
+function randomId() {
+    return String(Math.floor(Math.random() * 10000000));
+}
+
 
 export default class ListContainer extends LightningElement {
 
-    @track list;                    // List of records to display
-    @api type;                      // Education/Experience
+    @track list;
+
+    get type() {
+        return this._type;
+    }                    
+    
+    // List of records to display
+    @api set type(input) {
+        switch (input) {
+            case 'Education':
+                this._type = input;
+                break;
+            case 'Experience':
+                this._type = input;
+                break;
+            default:
+                console.log('Unsupported list type: ' + input)
+                break;
+        }
+    }                      // Education/Experience
     isEditing = false;              // 
     isCreating = false;             // 
 
+
+    connectedCallback() {
+        this.list = getMockData(5);
+        console.log(this.list);
+    }
     /**
      *  Wire service method that passes in 'type' and retrieves list of that obj
      */
-    @wire(getListOf, { type: this.type })
+    // @wire(getListOf, { type: this.-type })
     getList({ error, data }) {
         if (error) {
             // Handle error
@@ -26,7 +69,9 @@ export default class ListContainer extends LightningElement {
         this.isCreating = true;
     }
 
+    // When a record is edited/added, refresh list
+    // Listens for onclick event on record-edit-form submit button
     onSubmitHandler() {
-        refreshApex(this.list);
+        // refreshApex(this.list);
     }
 }
